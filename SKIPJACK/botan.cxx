db1055cbabc49d06cfa1f4fe8ded3ede49a08555
Jeffrey Walton <noloader@gmail.com>
2019-04-27 14:22:23 -0400

    Update Botan test program

--- SKIPJACK/botan.cxx
+++ SKIPJACK/botan.cxx
@@ -12,36 +12,67 @@
 #include <memory>
 #include <cstdint>
 
-void GenerateTestVector(
-    const uint8_t k[10],
-    const uint8_t p[8],
-    uint8_t c[8])
+void GenerateTestVector(size_t plainLen)
 {
     using namespace Botan;
     static size_t count=0;
 
-    ECB_Encryption enc(new Skipjack, new Null_Padding);
+    AutoSeeded_RNG rng;
+    CBC_Encryption enc(new Skipjack, new Null_Padding);
 
-    secure_vector<uint8_t> key(k, k+10);
-    secure_vector<uint8_t> plain(p, p+8);
-    secure_vector<uint8_t> cipher(8);
+    secure_vector<uint8_t> key = rng.random_vec(10);
+    secure_vector<uint8_t> iv = rng.random_vec(8);
+    secure_vector<uint8_t> plain = rng.random_vec(plainLen);
+    secure_vector<uint8_t> cipher(plain);
 
     enc.set_key(key);
-    enc.start(NULL, 0);
+    enc.start(iv.data(), iv.size());
     enc.finish(cipher);
 
     static bool once = false;
     if (once == false)
     {
-        std::cout << "AlgorithmType: SymmetricCipher" << std::endl;
-        std::cout << "Name: SKIPJACK/ECB" << std::endl;
-        std::cout << "Source: SP800-17, Table 6, pp. 140-42" << std::endl;
+        //std::cout << "AlgorithmType: SymmetricCipher" << std::endl;
+        std::cout  << std::endl;
+        std::cout << "Name: SKIPJACK/CBC" << std::endl;
+        std::cout << "Source: Botan library v1.11.7" << std::endl;
         once = true;
     }
 
+#if 0
+    if (count == 0)
+    {
+        // NIST test vector 0 from SP800-17
+        std::cout << "#" << std::endl;
+        std::cout << "Comment: Test " << count << std::endl;
+        std::cout << "Key: " << "80000000000000000000" << std::endl;
+        std::cout << "IV: " << "0000000000000000" << std::endl;
+        std::cout << "Plaintext: " << "0000000000000000" << std::endl;
+        std::cout << "Ciphertext: " << "7A00E49441461F5A" << std::endl;
+        std::cout << "Test: Encrypt" << std::endl;
+        
+        count++;
+    }
+
+    if (count == 1)
+    {
+        // NIST test vector 79 from SP800-17
+        std::cout << "#" << std::endl;
+        std::cout << "Comment: Test " << count << std::endl;
+        std::cout << "Key: " << "00000000000000000001" << std::endl;
+        std::cout << "IV: " << "0000000000000000" << std::endl;
+        std::cout << "Plaintext: " << "0000000000000000" << std::endl;
+        std::cout << "Ciphertext: " << "74D0E7C2E3B450A8" << std::endl;
+        std::cout << "Test: Encrypt" << std::endl;
+        
+        count++;
+    }
+#endif
+
     std::cout << "#" << std::endl;
-    std::cout << "Comment: Round " << count << " known answer" << std::endl;
+    std::cout << "Comment: Test " << count << std::endl;
     std::cout << "Key: " << hex_encode(key) << std::endl;
+    std::cout << "IV: " << hex_encode(iv) << std::endl;
     std::cout << "Plaintext: " << hex_encode(plain) << std::endl;
     std::cout << "Ciphertext: " << hex_encode(cipher) << std::endl;
     std::cout << "Test: Encrypt" << std::endl;
@@ -49,34 +80,14 @@ void GenerateTestVector(
     count++;
 }
 
-void shift_right(unsigned char v[10])
-{
-    for (size_t i=0; i<10; ++i)
-    {
-        if (v[i] == 0) {continue;}
-
-        unsigned int x = (v[i] >> 1);
-        unsigned int y = (v[i]  & 1) ? 0x80 : 0;
-        
-        v[i] = (unsigned char)x;
-        if (i < 9)
-            v[i+1] = (unsigned char)y;
-        
-        break;
-    }
-}
-
 int main()
 {
-    // NIST test vector
-    uint8_t k[10] = { 0x80,0, 0,0,0,0, 0,0,0,0 };
-    const uint8_t p[8] = { 0,0,0,0, 0,0,0,0 };
-    uint8_t c[8];
-    
-    for (size_t i=0; i<80; ++i)
+    for (size_t i=0+1; i<10+1; ++i)
     {
-        GenerateTestVector(k, p, c);
-        shift_right(k);
+        for (size_t j=0; j<8; ++j)
+        {
+            GenerateTestVector(i*8);
+        }
     }
     
     return 0;
