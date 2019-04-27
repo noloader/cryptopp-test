eca56b708ed2d93eb75b116575474b9f5fbf5614
Jeffrey Walton <noloader@gmail.com>
2019-04-27 13:25:30 -0400

    Update Botan test program

--- SKIPJACK/botan.cxx
+++ SKIPJACK/botan.cxx
@@ -10,54 +10,24 @@
 
 #include <iostream>
 #include <memory>
+#include <cstdint>
 
-// Random step
-uint8_t Step(uint8_t mask)
+void GenerateTestVector(
+    const uint8_t k[10],
+    const uint8_t p[8],
+    uint8_t c[8])
 {
     using namespace Botan;
-    AutoSeeded_RNG rng;
-
-    uint8_t b;
-    rng.randomize(&b, 1);
-    return b & mask;
-}
-
-void GenerateECBTest(size_t plainLen)
-{
-    using namespace Botan;
-    AutoSeeded_RNG rng;
     static size_t count=0;
 
     ECB_Encryption enc(new Skipjack, new Null_Padding);
 
-    secure_vector<uint8_t> key = rng.random_vec(10);
-
-    secure_vector<uint8_t> plain = rng.random_vec(plainLen);
-    secure_vector<uint8_t> cipher(plain.size());
-
-    if (count == 0)
-    {
-        // NIST test vector
-        const uint8_t k[] = { 0x80, 0,0,0,0,0,0,0,0,0 };
-        const uint8_t p[] = { 0,0,0,0 ,0,0,0,0 };
-
-        key = secure_vector<uint8_t>(k, k+sizeof(k));
-        plain = secure_vector<uint8_t>(p, p+sizeof(p));
-        cipher.resize(plain.size());
-    }
-    else if (count == 1)
-    {
-        const uint8_t k[] = { 0x00, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11 };
-        const uint8_t p[] = { 0x33, 0x22, 0x11, 0x00, 0xdd, 0xcc, 0xbb, 0xaa };
-
-        key = secure_vector<uint8_t>(k, k+sizeof(k));
-        plain = secure_vector<uint8_t>(p, p+sizeof(p));
-        cipher.resize(plain.size());
-    }
+    secure_vector<uint8_t> key(k, k+10);
+    secure_vector<uint8_t> plain(p, p+8);
+    secure_vector<uint8_t> cipher(8);
 
     enc.set_key(key);
     enc.start(NULL, 0);
-
     enc.finish(cipher);
 
     static bool once = false;
@@ -65,11 +35,12 @@ void GenerateECBTest(size_t plainLen)
     {
         std::cout << "AlgorithmType: SymmetricCipher" << std::endl;
         std::cout << "Name: SKIPJACK/ECB" << std::endl;
-        std::cout << "Source: Botan 1.11.7 test program" << std::endl;
+        std::cout << "Source: SP800-17, Table 6, pp. 140-42" << std::endl;
         once = true;
     }
 
     std::cout << "#" << std::endl;
+    std::cout << "Comment: Round " << count << " known answer" << std::endl;
     std::cout << "Key: " << hex_encode(key) << std::endl;
     std::cout << "Plaintext: " << hex_encode(plain) << std::endl;
     std::cout << "Ciphertext: " << hex_encode(cipher) << std::endl;
@@ -78,87 +49,35 @@ void GenerateECBTest(size_t plainLen)
     count++;
 }
 
-void GenerateCBCTest(size_t plainLen)
+void shift_right(unsigned char v[10])
 {
-    using namespace Botan;
-    AutoSeeded_RNG rng;
-    static size_t count=0;
-
-    CBC_Encryption enc(new Skipjack, new Null_Padding);
-
-    secure_vector<uint8_t> key = rng.random_vec(10);
-    secure_vector<uint8_t> iv = rng.random_vec(8);
-
-    secure_vector<uint8_t> plain = rng.random_vec(plainLen);
-    secure_vector<uint8_t> cipher(plain.size());
-
-    if (count == 0)
+    for (size_t i=0; i<10; ++i)
     {
-        // NIST test vector
-        const uint8_t k[] = { 0x80, 0,0,0,0,0,0,0,0,0 };
-        const uint8_t v[] = { 0,0,0,0 ,0,0,0,0 };
-        const uint8_t p[] = { 0,0,0,0 ,0,0,0,0 };
+        if (v[i] == 0) {continue;}
 
-        key = secure_vector<uint8_t>(k, k+sizeof(k));
-        iv = secure_vector<uint8_t>(v, v+sizeof(v));
-        plain = secure_vector<uint8_t>(p, p+sizeof(p));
-        cipher.resize(plain.size());
+        unsigned int x = (v[i] >> 1);
+        unsigned int y = (v[i]  & 1) ? 0x80 : 0;
+        
+        v[i] = (unsigned char)x;
+        if (i < 9)
+            v[i+1] = (unsigned char)y;
+        
+        break;
     }
-    else if (count == 1)
-    {
-        const uint8_t k[] = { 0x00, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11 };
-        const uint8_t v[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-        const uint8_t p[] = { 0x33, 0x22, 0x11, 0x00, 0xdd, 0xcc, 0xbb, 0xaa };
-
-        key = secure_vector<uint8_t>(k, k+sizeof(k));
-        iv = secure_vector<uint8_t>(v, v+sizeof(v));
-        plain = secure_vector<uint8_t>(p, p+sizeof(p));
-        cipher.resize(plain.size());
-    }
-
-    enc.set_key(key);
-    enc.start(iv.data(), iv.size());
-
-    enc.finish(cipher);
-
-    static bool once = false;
-    if (once == false)
-    {
-        std::cout << "AlgorithmType: SymmetricCipher" << std::endl;
-        std::cout << "Name: SKIPJACK/CBC" << std::endl;
-        std::cout << "Source: Botan 1.11.7 test program" << std::endl;
-        once = true;
-    }
-
-    std::cout << "#" << std::endl;
-    std::cout << "Key: " << hex_encode(key) << std::endl;
-    std::cout << "IV: " << hex_encode(iv) << std::endl;
-    std::cout << "Plaintext: " << hex_encode(plain) << std::endl;
-    std::cout << "Ciphertext: " << hex_encode(cipher) << std::endl;
-    std::cout << "Test: Encrypt" << std::endl;
-    
-    count++;
 }
 
 int main()
 {
-    for (size_t i=8; i<128; i+=8)
+    // NIST test vector
+    uint8_t k[10] = { 0x80,0, 0,0,0,0, 0,0,0,0 };
+    const uint8_t p[8] = { 0,0,0,0, 0,0,0,0 };
+    uint8_t c[8];
+    
+    for (size_t i=0; i<80; ++i)
     {
-        for (size_t j=0; j++<8; j++)
-        {
-            GenerateECBTest(i);
-        }
+        GenerateTestVector(k, p, c);
+        shift_right(k);
     }
-
-    std::cout << std::endl;
-
-    for (size_t i=8; i<128; i+=8)
-    {
-        for (size_t j=0; j++<8; j++)
-        {
-            GenerateCBCTest(i);
-        }
-    }
-
-    std::cout << std::endl;
+    
+    return 0;
 }
